fc6ff97362b541d1ea8f89c11e0a2622ee76cd03
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-07-30 14:03:14 -0700

    updated idb.js

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -1,74 +1,70 @@
 let db;
-const request = indexedDB.open();
 
-request.onupgradeneeded = function(event) {
+// request new "budget" db
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+  //create "pending" object store
   const db = event.target.result;
-  db.createObjectStore('', { autoIncrement: true });
+  db.createObjectStore("pending", { autoIncrement: true });
 };
 
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
   // when db is successfully created with its object store (from onupgradedneeded event above), save reference to db in global variable
   db = event.target.result;
 
   // check if app is online, if yes run checkDatabase() function to send all local db data to api
   if (navigator.onLine) {
-    uploadSomething();
+    checkDatabase();
   }
 };
 
-request.onerror = function(event) {
+request.onerror = function (event) {
   // log error here
   console.log(event.target.errorCode);
 };
 
 function saveRecord(record) {
-  const transaction = db.transaction(['new_pizza'], 'readwrite');
+  const transaction = db.transaction(["pending"], "readwrite");
 
-  const pizzaObjectStore = transaction.objectStore('new_pizza');
+  const store = transaction.objectStore("pending");
 
   // add record to your store with add method.
-  pizzaObjectStore.add(record);
+  store.add(record);
 }
 
-function uploadPizza() {
+function checkDatabase() {
   // open a transaction on your pending db
-  const transaction = db.transaction(['new_pizza'], 'readwrite');
+  const transaction = db.transaction(["pending"], "readwrite");
 
   // access your pending object store
-  const pizzaObjectStore = transaction.objectStore('new_pizza');
+  const store = transaction.objectStore("pending");
 
   // get all records from store and set to a variable
-  const getAll = pizzaObjectStore.getAll();
+  const getAll = store.getAll();
 
-  getAll.onsuccess = function() {
-    // if there was data in indexedDb's store, let's send it to the api server
+  getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
-      fetch('/api/pizzas', {
-        method: 'POST',
+      fetch("/api/transaction/bulk", {
+        method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
         }
       })
         .then(response => response.json())
-        .then(serverResponse => {
-          if (serverResponse.message) {
-            throw new Error(serverResponse);
-          }
-
-          const transaction = db.transaction(['new_pizza'], 'readwrite');
-          const pizzaObjectStore = transaction.objectStore('new_pizza');
-          // clear all items in your store
-          pizzaObjectStore.clear();
-        })
-        .catch(err => {
-          // set reference to redirect back here
-          console.log(err);
-        });
+        .then(() => {
+        //if successful. open transaction on pending db
+        const transaction = db.transaction(["pending"], "readwrite");
+        //access pending object store
+        const store = transaction.objectStore("pending");
+        // clear all items in your store
+        store.clear();
+      });
     }
   };
 }
 
 // listen for app coming back online
-window.addEventListener('online', uploadPizza);
+window.addEventListener("online", checkDatabase);
