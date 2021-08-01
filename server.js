a89c22dc6096ff0073d288cfb8ec7ee8c6e57ebe
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-08-01 15:08:06 -0700

    reworking server.js

--- server.js
+++ server.js
@@ -2,22 +2,27 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const PORT = process.env.PORT || 3001;
+
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(logger("dev"));
+
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 // routes
 app.use(require("./routes/api.js"));
