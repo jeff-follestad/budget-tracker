78fa96679e5dd8a3e98083138fca96846ca875ed
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-08-01 16:52:41 -0700

    index.js

--- public/index.js
+++ public/index.js
@@ -20,6 +20,8 @@ fetch("/api/transaction")
 
 function populateTotal() {
   // reduce transaction amounts to a single total value
+  console.log(transactions);
+  
   let total = transactions.reduce((total, t) => {
     return total + parseInt(t.value);
   }, 0);
