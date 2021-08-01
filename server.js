e8fa59c4382d3fe2127e4675c579e7e7c58bbd85
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-08-01 14:06:05 -0700

    Deploying

--- server.js
+++ server.js
@@ -2,24 +2,21 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+  useFindAndModify: false,
   useNewUrlParser: true,
-  useFindAndModify: false
-});
+  useUnifiedTopology: true
+})
 
 // routes
 app.use(require("./routes/api.js"));
