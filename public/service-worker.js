060887dd00f07e632467475803dadfbd41453ae3
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-08-01 17:09:51 -0700

    Fix typo in service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,15 +7,6 @@ const FILES_TO_CACHE = [
   "/db.js"
 ];
 
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/index.js",
-  "/manifest.json",
-  "/db.js"
-];
-
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 self.addEventListener("install", function (evt) {
