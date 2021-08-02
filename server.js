960dd1ce78b9443879927d8a450ace28718f1b71
Jeff Follestad <jeffrey.follestad@gmail.com>
2021-08-01 17:26:24 -0700

    Add database connection error logging

--- server.js
+++ server.js
@@ -15,14 +15,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget",
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/budget",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
         useFindAndModify: false
     }
-);
+  )
+  .then(() => console.log("Database connected!"))
+  .catch(err => console.log("Database connection failed: " + err));
 
 // routes
 app.use(require("./routes/api.js"));
